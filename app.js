20bc818b24e5339ee306a6d9713c7c10b2a88dbd
Anastasiya Fedotova <nastenaf@list.ru>
2022-10-05 13:47:53 +0300

    Подключение к mogoose

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+/* eslint-disable no-console */
 const express = require('express');
+const mongoose = require('mongoose');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 const app = express();
 
-app.listen(PORT);
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/moviesdb', {
+    useNewUrlParser: true,
+  });
+  await app.listen(PORT);
+  // eslint-disable-next-line no-console
+  console.log(`App listening on port ${PORT}`);
+}
+main();
