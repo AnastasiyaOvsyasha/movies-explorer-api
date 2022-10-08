a30e1613b4168c3281f4051b6f219e91dc762761
Anastasiya Fedotova <nastenaf@list.ru>
2022-10-08 14:59:09 +0300

    changing eslint directives

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,22 +3,19 @@ const AuthorizationError = require('../errors/AuthorizationError');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-// eslint-disable-next-line consistent-return
-module.exports.auth = (req, res, next) => {
-  if (req.cookies.jwt) {
-    const token = req.cookies.jwt;
-    let payload;
+const auth = (req, res, next) => {
+  const token = req.cookies.jwt;
+  let payload;
 
-    try {
-      payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key');
-    } catch (err) {
-      return next(new AuthorizationError('Необходимо авторизоваться'));
-    }
-
-    req.user = payload;
-
-    next();
-  } else {
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key');
+  } catch (err) {
     return next(new AuthorizationError('Необходимо авторизоваться'));
   }
+
+  req.user = payload;
+
+  return next(new AuthorizationError('Необходимо авторизоваться'));
 };
+
+module.exports = auth;
