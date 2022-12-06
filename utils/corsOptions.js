0b9b0145f94cf272e35d4cd5e2360a7707558f98
Anastasiya Fedotova <nastenaf@list.ru>
2022-12-06 13:51:20 +0300

    server

--- utils/corsOptions.js
+++ utils/corsOptions.js
@@ -1,8 +1,8 @@
 const corsOptions = {
   origin: [
-    // 'http://localhost:3000',
-    'http://diploma.praktikum.nomoredomains.icu',
-    'https://diploma.praktikum.nomoredomains.icu',
+    'http://localhost:3000',
+    //'http://diploma.praktikum.nomoredomains.icu',
+    //'https://diploma.praktikum.nomoredomains.icu',
   ],
   credentials: true,
   optionsSuccessStatus: 200,
